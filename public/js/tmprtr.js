267af4dfcbf1f3b6aad023d999ffc07fa338c28d
Ryan Seys <rsmseys@gmail.com>
2013-02-01 23:34:52 -0500

    Update to icon choices

--- public/js/tmprtr.js
+++ public/js/tmprtr.js
@@ -4,7 +4,7 @@ icon = document.getElementById('icon');
 
 function showPosition(position) {
   $.post(
-    '/tmprtr', 
+    '/tmprtr',
     data = {lat: position.coords.latitude, lon: position.coords.longitude },
     success = function(values, status, jqXHR) {
       icon.setAttribute('data-icon', getIconCharacter(values.icon)); //set the icon
@@ -16,12 +16,129 @@ function showPosition(position) {
 
 function getIconCharacter(icon) {
   switch(icon) {
-    case 'nt_partlycloudy':
+    case 'nt_chanceflurries' :
+      return 'V';
+    break;
+    case 'nt_chancerain' :
+      return 'Q';
+    break;
+    case 'nt_chancesleet' :
+      return 'T';
+    break;
+    case 'nt_chancesnow' :
+      return 'V';
+    break;
+    case 'nt_chancetstorms' :
+      return '0';
+    break;
+    case 'nt_clear' :
+      return 'C';
+    break;
+    case 'nt_cloudy' :
+      return 'Y';
+    break;
+    case 'nt_flurries' :
+      return 'W';
+    break;
+    case 'nt_fog' :
+      return 'K';
+    break;
+    case 'nt_hazy' :
+      return 'E';
+    break;
+    case 'nt_mostlycloudy' :
+      return 'N';
+    break;
+    case 'nt_mostlysunny' :
       return 'I';
+    break;
+    case 'nt_partlycloudy' :
+      return 'I';
+    break;
+    case 'nt_partlysunny' :
+      return 'N';
+    break;
+    case 'nt_rain' :
+      return 'R';
+    break;
+    case 'nt_sleet' :
+      return '';
+    break;
+    case 'nt_snow' :
+      return 'X';
+    break;
+    case 'nt_sunny' :
+      return 'C';
+    break;
+    case 'nt_tstorms' :
+      return 'O';
+    break;
+    case 'nt_unknown' :
+      return ')';
+    break;
+    case 'chanceflurries' :
+      return 'V';
+    break;
+    case 'chancerain' :
+      return 'Q';
+    break;
+    case 'chancesleet' :
+      return 'T';
+    break;
+    case 'chancesnow' :
+      return 'V';
+    break;
+    case 'chancetstorms' :
+      return '0';
+    break;
+    case 'clear' :
+      return 'B';
+    break;
+    case 'cloudy' :
+      return 'Y';
+    break;
+    case 'flurries' :
+      return 'W';
+    break;
+    case 'fog' :
+      return 'M';
+    break;
+    case 'hazy' :
+      return 'J';
+    break;
+    case 'mostlycloudy' :
+      return 'N';
+    break;
+    case 'mostlysunny' :
+      return 'H';
+    break;
+    case 'partlycloudy' :
+      return 'H';
+    break;
+    case 'partlysunny' :
+      return 'N';
+    break;
+    case 'rain' :
+      return 'R';
+    break;
+    case 'sleet' :
+      return 'U';
+    break;
+    case 'snow' :
+      return 'X';
+    break;
+    case 'sunny' :
+      return 'B';
+    break;
+    case 'tstorms' :
+      return 'Z';
+    break;
+    case 'unknown' :
+      return ')';
     break;
 
     default:
-      return "-";
+      return ")";
     break;
   }
 }
